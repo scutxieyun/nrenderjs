a759f59c4879c43495a59ad41f1d1760cf7e4bc8
scutxieyun <scutxieyun@hotmail.com>
2016-05-28 22:24:18 +0800

    commit the import change for convert

--- src/tools/json2jsx.js
+++ src/tools/json2jsx.js
@@ -14,6 +14,7 @@ var imgTemplate = _.template('<MeImage src="<%= src%>" displayType = {<%= displa
 var grpTemplate = _.template("<MeDiv displayType = {<%= displayType%>} pageIdx={<%= pageIdx %>} cxt={cxt} id={<%= id%>} style={{<%= style %>}}><%= children%></MeDiv>");
 var divTemplate = _.template('<div style={{<%= style %>}}><%= content%></div>')
 var animationTemplate = _.template('<MeAnimation displayType = {<%= displayType%>} pageIdx={<%= pageIdx %>} cxt={cxt} animationClass="<%= animationClass%>" animation={<%= animation%>} normalStyle={{<%= normalStyle%>}}><%= children %></MeAnimation>');
+var touchTriggerTemplate = _.template('<MeTouchTrigger pageIdx={<%= pageIdx %>} cxt={cxt} id={<%= id %>} normalStyle={{<%= style%>}} triggerActions={{"<%= triggerActions.evt %>":<%= triggerActions.cmds %>}}><%= children %></MeTouchTrigger>');
 
 
 var itemFuncMap = {
@@ -52,6 +53,7 @@ function renderPage(page){
 			grpDiv = renderItem(page,grp.owner,newGrps);
 			items.push(grpDiv);
 		}else{
+			items = items.concat(newGrps);
 		}
 	});
 	page.children = items.join("\n");
@@ -85,25 +87,106 @@ function noTypeDefined(page,item){
 	return NoTypeDefinedT({item_type:item.item_type,style:_style});
 }
 
+function imgRenderItem(page,item,_style){
+	return imgTemplate({src:item.item_val,displayType:item.item_display_status,style:_style.join(",")});
+}
+
+function textRenderItem(page,item,_style){
+	_stype.push(fontStyleTemplate(item));
+	return divTemplate({content:item.item_val,displayType:item.item_display_status,style:_style.join(",")});
+}
+
+function grpRenderItem(page,item,_style,content){
+	_style.push(fontStyleTemplate(item));
+	return grpTemplate({displayType:item.item_display_status,
+						pageIdx:page.idx,
+						id:item.item_id,
+						style:_style,
+						children:content});
+}
+function renderItem(page,item,content){
+	var cmds = [];
 	var _style = [posStyleTemplate(item),sizeStyleTemplate(item)];
-	var otherStyle = [];
-	if(item.item_opacity != 100){}
-	
-	if(item.item_animation == null || item.item_animation === "" || item.item_animation === "none"){
-		return imgTemplate({src:item.item_val,style:_style.join(",")});
-	}else{
-		var animation = item.item_animation_val != "" ? item.item_animation_val
+	var animation = null;
+	item.pageIdx = page.idx;
+	if(item.item_href != null && item.item_href != ""){
+		//hide_el:-2|hide_el:65185725
+		cmds = convertOldCmd(item.item_href);
+	}
+	if(!(item.item_animation == null || item.item_animation === "" || item.item_animation === "none")){
+		animation = item.item_animation_val != "" ? item.item_animation_val
 				.replace(/delay/,"animationDelay")
 				.replace(/duration/,"animationDuration")
 				.replace(/infinite/,"animationIterationCount") : defaultAnimation;
+	}
+	
+	var funcKey = item.item_type.toString();
+	var renderFunc = itemFuncMap[funcKey]
+	
+	if(renderFunc == undefined ){
+		return NoTypeDefinedT({item_type:item.item_type,style:_style});
+	}
+	
+	if(animation == null && cmds.length == 0){
+		return renderFunc(page,item,_style,content)
+	}
+	
+	var _itemContent = renderFunc(page,item,[],content);//_style将放在外围
+	
+	if(cmds.length == 0 && animation != null){
 		return animationTemplate({animationClass:item.item_animation,
+								  children:_itemContent,
 								  animation:animation,
 								  normalStyle:_style.join(","),
 								  pageIdx:page.idx,
+								  displayType:item.item_display_status,
+								  id:item.item_id});
 	}
+	if(animation != null){
+		_itemContent = animationTemplate({animationClass:item.item_animation,
+								  children:_itemContent,
 								  animation:animation,
+								  normalStyle:"",
 								  pageIdx:page.idx,
+								  displayType:0,
+								  id:item.item_id});
 	}
+	if(cmds.length > 0){
+		return touchTriggerTemplate({
+			style:_style,
+			children:_itemContent,
+			pageIdx:page.idx,
+			id:item.item_id,
+			triggerActions:{evt:"tap",
+							cmds:"[" + cmds.join(",") + "]"
+						}
+		});
+	}
+	return "";
+	
+	function convertOldCmd(item_href){
+		var _cmdMap ={
+			"hide_el":["componentDo","hide"],
+			"show_el":["componentDo","show"],
+		}
+		var actionTemplate = _.template('{action:"<%= cmd %>",propagate:<%= propagate%>}');
+			//hide_el:-2|hide_el:65185725
+		var _cmds = item_href.split("|");
+		var res = [];
+		_.each(_cmds,function(cmd){
+			var args = cmd.split(":");
+			var new_cmd = _cmdMap[args[0]];
+			var resStr = "";
+			if(new_cmd != undefined){
+				args.splice(0,1);
+				new_cmd = new_cmd.concat(args);
+				var _method = new_cmd[0];
+				new_cmd.splice(0,1);
+				resStr = _method + "(" + new_cmd.join(",") + ")";
+				res.push(actionTemplate({cmd:resStr,propagate:true}));
+			}
+		});
+		return res;
 	}
 }
 	
